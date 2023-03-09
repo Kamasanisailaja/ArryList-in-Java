b16a43f26ff00baecac0f262fb17f75914f39bb3
Kamasanisailaja <106640335+Kamasanisailaja@users.noreply.github.com>
2023-03-09 08:38:53 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Arraylist {
+    public static void main(String args[])
+    {
+        ArrayList<String> friends=new ArrayList<String>();
+        friends.add("sailaja");
+        friends.add("yamini");
+        friends.add("zuzubee");
+        System.out.println(friends);
+    }
+}
